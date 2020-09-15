1e7f08bc4f666586db9bb251abe6e1df83cc051d
Enkhlen Khurelbaatar <enkhulek@gmail.com>
2020-09-14 17:38:37 -0700

    Can add new notes and delete existing notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateNote from "./CreateNote";
+import { v4 as uuidv4} from "uuid";
 
-function App(){
-    return (
-        <div>
-            <Header />
-            {notes.map( note => (
-                <Note 
-                    key={note.key}
-                    title = {note.title}
-                    body = {note.content}
-                />
-            ))}
-            <Footer />
-        </div>
-    );
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+     
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {     
+        return index !==id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateNote clickAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={uuidv4()}
+          title={note.title}
+          body={note.content}
+          id = {index}
+          onDelete={deleteNote}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
